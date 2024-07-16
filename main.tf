dd80ac6316e53da80ed7a9d40748eca871211d8e
pooja <poojaskumara@gmail.com>
2024-07-16 16:36:28 +0530

    kubernetes

--- main.tf
+++ main.tf
@@ -53,3 +53,19 @@ resource "aws_iam_openid_connect_provider" "eks" {
 
   thumbprint_list = [data.external.oidc-thumbprint.result.thumbprint]
 }
+
+# resource "aws_eks_access_entry" "ci-server" {
+#   cluster_name      = aws_eks_cluster.main.name
+#   principal_arn     = "arn:aws:iam::739561048503:role/ci-server-role"
+#   type              = "STANDARD"
+# }
+#
+# resource "aws_eks_access_policy_association" "ci-server-policy" {
+#   cluster_name  = aws_eks_cluster.main.name
+#   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+#   principal_arn = "arn:aws:iam::739561048503:role/ci-server-role"
+#
+#   access_scope {
+#     type       = "cluster"
+#   }
+# }
