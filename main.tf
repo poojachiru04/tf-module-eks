e6ef99bfaf62d93946c36b0ae60fe3f3cab87832
pooja <poojaskumara@gmail.com>
2024-07-17 16:45:15 +0530

    kubernetes

--- main.tf
+++ main.tf
@@ -60,20 +60,20 @@ resource "aws_iam_openid_connect_provider" "eks" {
   thumbprint_list = [data.external.oidc-thumbprint.result.thumbprint]
 }
 
-#
-# resource "aws_eks_access_entry" "ci-server" {
-#   cluster_name      = aws_eks_cluster.main.name
-#   principal_arn     = "arn:aws:iam::739561048503:role/ci-server-role"
-#   type              = "STANDARD"
-# }
-#
-# resource "aws_eks_access_policy_association" "ci-server-policy" {
-#   cluster_name  = aws_eks_cluster.main.name
-#   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-#   principal_arn = "arn:aws:iam::739561048503:role/ci-server-role"
-#
-#   access_scope {
-#     type       = "cluster"
-#   }
-# }
+
+resource "aws_eks_access_entry" "ci-server" {
+  cluster_name      = aws_eks_cluster.main.name
+  principal_arn     = "arn:aws:iam::339712755999:role/ci-server-role"
+  type              = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "ci-server-policy" {
+  cluster_name  = aws_eks_cluster.main.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = "arn:aws:iam::339712755999:role/ci-server-role"
+
+  access_scope {
+    type       = "cluster"
+  }
+}
 
